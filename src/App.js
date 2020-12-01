f11db421b861b330056f26bf6497dd81762bd964
Anton Smirnov <antosha-85@yandex.ru>
2020-11-30 23:05:27 -0500

    Installed react-router-dom and wrapped everything in BrowserRouter and assigned Links for each components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3">
-            <Sidebar />
-          </div>
-          <div className="col-lg-9 app__main-content">
-            {/* Navbar */}
-            <Navbar />
+    <Router>
+      <div className="app">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
+              <Sidebar />
+            </div>
+            <div className="col-lg-9 app__main-content">
+              {/* Navbar */}
+              <Navbar />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
