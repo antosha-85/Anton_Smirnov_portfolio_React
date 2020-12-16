73aae5307a519afed969a467c6e199d7373f8366
Anton Smirnov <antosha-85@yandex.ru>
2020-12-16 09:11:29 -0500

    Created a few divs for my navbar inside the Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import ProjectCard from './ProjectCard'
-import data_projects from './projects_data/projects_data'
-
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import data_projects from "./projects_data/projects_data";
 
 const Projects = () => {
-    return (
-        <div className="container projects">
-            <div className="row">
-                {data_projects.map(project => (
-                    <ProjectCard key={project.name} project={project}/>
-                ))
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="container projects">
+      <div className="projects__navbar">
+        <div>All</div>
+        <div>React</div>
+        <div>Redux</div>
+        <div>Firebase</div>
+        <div>Vanilla JS</div>
+      </div>
+      <div className="row">
+        {data_projects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
