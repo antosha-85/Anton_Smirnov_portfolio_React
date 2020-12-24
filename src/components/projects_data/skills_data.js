dd91784cb53c96f8144f927412b1fb774492c97f
Anton Smirnov <antosha-85@yandex.ru>
2020-12-24 11:08:03 -0500

    Converted my skills variable into a JSX array to highlight some elements using strong tag

--- src/components/projects_data/skills_data.js
+++ src/components/projects_data/skills_data.js
@@ -1,32 +1,33 @@
 import api from "../../assets/icons/api.svg";
+import React from 'react'
 
 const skills = [
   {
     icon: api,
     title: "Frontend Development",
-    about: "I can build beautiful and scalable SPAs using HTML, CSS, and React",
+    about: ["I can build beautiful and scalable SPAs using ", <strong>HTML</strong>, ", " , <strong>CSS</strong>,  ", " , <strong>JavaScript</strong>, " and ",  <strong>React</strong>],
   },
   {
     icon: api,
     title: "Backend Development",
-    about: "I can handle databases, APIs using PostgreSQL",
+    about: ["I can handle ", <strong>databases</strong>, ", " ,<strong>REST APIs</strong>, " using ", <strong>PostgreSQL</strong>],
   },
   {
     icon: api,
     title: "API Development",
-    about: "I can develop REST APIs using Node",
+    about: ["I can develop ", <strong>REST APIs</strong>,  " using ", <strong>Node</strong>],
   },
   {
     icon: api,
     title: "Competitive Coder",
     about:
-      "I can resolve different algorithms, which I train to do on Leetcode",
+      ["I can resolve different algorithms, which I train on ", <strong>Leetcode</strong>],
   },
   {
     icon: api,
     title: "Work in an Agile Environment",
     about:
-      "I can work in an Agile Environment, I'm familiar with Jira, Stand ups, Sprints, etc.",
+  ["I can work in an ", <strong>Agile</strong>, " Environment, I'm familiar with ", <strong>Jira</strong>, " ,", "Stand ups, Sprints, etc."],
   },
 ];
 
