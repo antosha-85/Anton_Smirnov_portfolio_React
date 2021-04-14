e311e479cb217a662241a6b1756d686f3125fe4c
antosha-85 <antosha-85@yandex.ru>
2021-04-14 12:08:31 -0600

    Fixed the issue with the background of bar overlying an icon for each row

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -4,9 +4,9 @@ const Bar = ({ value: { icon, name, level } }) => {
   const bar_width = `${level}%`;
 
   return (
-    <div className="bar">
-      <div className="row">
-        <img src={icon} alt="icon" className="bar__icon mr-1" />
+    <div className="row">
+      <img src={icon} alt="icon" className="bar__icon mr-1" />
+      <div className="bar">
         <div className="bar__wrapper" style={{ width: bar_width }}>
           <span className="bar__name">{name}</span>
         </div>
